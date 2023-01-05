f63571fc27b3462b33f7fef55f3ef5b421899712
Miorka <98622937+Miorka@users.noreply.github.com>
2023-01-05 14:29:39 +0300

    Delete Homework_Lesson1/Task1 directory

--- Homework_Lesson1/Task1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите 2 числа");
-int numderA = Convert.ToInt32(Console.ReadLine());
-int numderB = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(Math.Max(numderA, numderB));
