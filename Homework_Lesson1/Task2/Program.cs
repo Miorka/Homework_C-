e4bd4ec0c82bca9072e8154234b56145e582852e
NastyaS <nastema@bk.ru>
2023-01-05 15:36:15 +0300

    Task 2 completed

---
+++ Homework_Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int numberMax = numberA;
+
+if (numberMax < numberB)
+{
+    numberMax = numberB;
+}
+
+if ( numberMax < numberC)
+{
+    numberMax = numberC;
+}
+
+Console.WriteLine(numberMax);
