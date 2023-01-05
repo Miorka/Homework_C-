0f9487ecb2e4195df3d23c4e1ffa24a3eb94c735
NastyaS <nastema@bk.ru>
2023-01-05 14:36:12 +0300

    Task1 completed

---
+++ Homework_Lesson1/Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите 2 числа");
+int numderA = Convert.ToInt32(Console.ReadLine());
+int numderB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Max(numderA, numderB));
