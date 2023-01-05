a1f13433346adf52916aba2666a6c26b663af37d
NastyaS <nastema@bk.ru>
2023-01-05 16:05:12 +0300

    Task 4 completed

---
+++ Homework_Lesson1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+do 
+{
+    if ((count % 2) == 0)
+    {
+        Console.Write($"{count} ");
+        
+    }
+    count++;
+}
+while (count <= number);
