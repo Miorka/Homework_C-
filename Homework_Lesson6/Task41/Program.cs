09a453f5d81f4f0825b4084bda3d0b4a9310006e
NastyaS <nastema@bk.ru>
2023-01-06 17:31:11 +0300

    homework for lesson 6 completed

---
+++ Homework_Lesson6/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите числа через символ ';'': ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(';'), int.Parse);
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {Count(arr)}");
+
+int Count(int[] array)
+{
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+return count;
+}
---
+++ Homework_Lesson6/Task43/Program.cs
@@ -0,0 +1,20 @@
+﻿double k1 = Func("Введите k1: ");
+double b1 = Func("Введите b1: ");
+double k2 = Func("Введите k2: ");
+double b2 = Func("Введите b2: ");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Точка пересечения имеет следующие координаты: ({x};{y})");
+
+double Func(string text)
+{
+    double number;
+    Console.Write(text);
+    number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
