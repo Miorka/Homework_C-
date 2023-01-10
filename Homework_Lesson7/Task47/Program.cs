88e17743a6242423cf16728ab87023a22209455d
NastyaS <nastema@bk.ru>
2023-01-10 17:32:22 +0300

    Task 47 completed

---
+++ Homework_Lesson7/Task47/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = InputNumber("Введите количество строк M =");
+int n = InputNumber("Введите количество столбцов N =");
+double[,] arr = new double[m,n];
+Random rand = new Random();
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = Convert.ToDouble(rand.Next(-100, 100)/10.0);
+        System.Console.Write(arr[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int InputNumber(string text)
+{
+ Console.Write(text);
+int number = Convert.ToInt32(Console.ReadLine());  
+return number; 
+}
+
